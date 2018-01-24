8b853c147b5ac031b447af609d26a48ae4602f9a
MalteMohr <35693217+MalteMohr@users.noreply.github.com>
2018-01-24 15:43:44 +0100

    test

--- src/main/java/com/innoq/lab/twaddle/TwaddleApplication.java
+++ src/main/java/com/innoq/lab/twaddle/TwaddleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TwaddleApplication {
 
+	// comment
 	public static void main(String[] args) {
 		SpringApplication.run(TwaddleApplication.class, args);
 	}
